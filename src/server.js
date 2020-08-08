692b2b27eee0b989889e03508ba1b67a35bb0bec
Davi <davi.silva41@fatec.sp.gov.br>
2020-08-08 01:01:33 -0300

    criando servidor local  com nodemon

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const server = express()
 
 server.use(express.static("public"))
+
 .get("/", (req, res) => {
     return res.sendFile(__dirname + "/views/index.html")
 })
 .get("/study", (req, res) => {
-    return res.send("Study")
+    return res.sendFile(__dirname + "/views/study.html")
 })
-.listen(5501)
+.get("/give-classes", (req, res) => {
+    return res.sendFile(__dirname + "/views/give-classes.html")
+})
+.listen(5500)/*teste*/
